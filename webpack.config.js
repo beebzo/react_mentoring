0efd6828df94578e16dbcfcbb75fb86ffba97864
Artur Galiakhmetov <artur_galiakhmetov@epam.com>
2022-10-24 21:18:28 +0300

    project_setup

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+};
+
+const config = {
+  entry: path.join(paths.SRC, 'index.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    host:'localhost',
+    port: 4000,
+    historyApiFallback: true,
+    open: true,
+    hot:true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: "commons",
+          chunks: "all",
+          minSize: 0,
+          minChunks: 2
+        }
+      }
+    }
+  }
+}
+
+module.exports = (env) => {
+  if (env.mode === 'dev') {
+    config.mode = 'development'
+  } else {
+    config.mode = 'production'
+  }
+  return config
+}
